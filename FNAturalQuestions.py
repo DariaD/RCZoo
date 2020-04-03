ddb663f82da8488206f900bc6767b4436203155c
Daria <daria.dzendzik@adaptcentre.ie>
2020-04-03 11:10:43 +0100

    update according to both datafiles

--- FNAturalQuestions.py
+++ FNAturalQuestions.py
@@ -9,6 +9,68 @@ from spacy.lang.en import English
 nlp = English()
 tokenizer = nlp.Defaults.create_tokenizer(nlp)
 
+def process_file(filename):
+    question_list, passage_list, answer_list = [], [], []
+    vocabulary, html_token_set = set(), set()
+    with gzip.open(filename) as f:
+        for line in f:
+            entry = json.loads(line)
+            passage_tokens = entry["document_tokens"]
+            passage_text_tokens = []
+            for passage_token in passage_tokens:
+                token = passage_token["token"]
+                if passage_token["html_token"]:
+                    html_token_set.add(passage_token["token"])
+                else:
+                    passage_text_tokens.append(token)
+
+            question_tokens = entry["question_tokens"]
+
+            question_list.append(len(question_tokens))
+            passage_list.append(len(passage_text_tokens))
+
+            answer = ""
+            for annotation in entry["annotations"]:
+                if annotation["yes_no_answer"] != "NONE":
+                    answer_list.append(1)
+                    answer = annotation["yes_no_answer"]
+                else:
+                    answer_annotation = annotation["short_answers"]
+                    if len(answer_annotation) == 0:
+                        answer_annotation = annotation["long_answer"]
+                        if answer_annotation["candidate_index"] == -1:
+                            continue
+                    else:
+                        answer_annotation = answer_annotation[0]
+                    answer_start = answer_annotation["start_token"]
+                    answer_end = answer_annotation["end_token"]
+                    answer_list.append(answer_end - answer_start + 1)
+
+            text_to_tokenize = " ".join(passage_text_tokens + question_tokens + [answer])
+            tokens = tokenizer(text_to_tokenize)
+            vocabulary.update({x.lemma_.lower() for x in tokens})
+            break
+
+    return  question_list, passage_list, answer_list, vocabulary, html_token_set
+
+
+def output(question_list, passage_list, answer_list, vocabulary, html_token_set):
+    print("Questions", question_list, len(question_list))
+    print("Passages", passage_list, len(passage_list))
+    print("Answers", answer_list, len(answer_list))
+    print("Vocab", len(vocabulary))
+
+    passage_avg = sum(passage_list) / len(passage_list)
+    avg_q = sum(question_list) / len(question_list)
+    avg_a = sum(answer_list) / len(answer_list)
+
+    # & # instances	& # passages &	# A/Q & AVG Q len	& AVG P len	 & AVG A len & Vcabulary Size
+    print("&".join([str(x) for x in ["", "-", len(passage_list), "-", avg_q, passage_avg, avg_a,
+                                     len(vocabulary)]]))
+
+    # print(vocabulary)
+    print(html_token_set)
+
 
 def get_all_examples(data_dir):
     question_list, passage_list, answer_list, instance_list = [], [], [], []
@@ -22,126 +84,89 @@ def get_all_examples(data_dir):
     for file in files:
         print(file)
         with gzip.open(folder+file) as f:
-            for line in f:
-                entry = json.loads(line)
-                passage_tokens = entry["document_tokens"]
-                passage_text_tokens = []
-                for passage_token in passage_tokens:
-                    # print(passage_token)
-                    # print(type(passage_token))
-                    token = passage_token["token"]
-                    if passage_token["html_token"]:
-                        html_token_set.add(passage_token["token"])
-                    else:
-                        passage_text_tokens.append(token)
+            q_list, p_list, a_list, vocab, html_set = process_file(folder + file)
+            question_list += q_list
+            passage_list += p_list
+            answer_list += a_list
+            vocabulary.update(vocab)
+            html_token_set.update(html_set)
 
-                question_tokens = entry["question_tokens"]
-
-                question_list.append(len(question_tokens))
-                passage_list.append(len(passage_text_tokens))
-
-
-
-                answer = ""
-                for annotation in entry["annotations"]:
-                    if annotation["yes_no_answer"] != "NONE":
-                        answer_list.append(1)
-                        answer = annotation["yes_no_answer"]
-                    else:
-                        answer_annotation = annotation["short_answers"]
-                        if len(answer_annotation) == 0:
-                            answer_annotation = annotation["long_answer"]
-                            if answer_annotation["candidate_index"] == -1:
-                                continue
-                        else:
-                            answer_annotation = answer_annotation[0]
-                        answer_start = answer_annotation["start_token"]
-                        answer_end = answer_annotation["end_token"]
-                        answer_list.append(answer_end - answer_start + 1)
-
-                #text_to_tokenize = " ".join([x["token"] for x in passage_tokens] + question_tokens + [answer])
-                text_to_tokenize = " ".join(passage_text_tokens + question_tokens + [answer])
-                tokens = tokenizer(text_to_tokenize)
-                vocabulary.update({x.lemma_.lower() for x in tokens})
-                break
-
-
-        print(question_list, len(question_list))
-        print(passage_list, len(passage_list))
-        print(answer_list, len(answer_list))
-        print(len(vocabulary))
-
-        passage_avg = sum(passage_list) / len(passage_list)
-        avg_q = sum(question_list) / len(question_list)
-        avg_a = sum(answer_list) / len(answer_list)
-
-        # & # instances	& # passages &	# A/Q & AVG Q len	& AVG P len	 & AVG A len & Vcabulary Size
-        print("&".join([str(x) for x in ["", "-", len(passage_list), "-", avg_q, passage_avg, avg_a,
-                         len(vocabulary)]]))
-
-        #print(vocabulary)
-        print(html_token_set)
+        output(question_list, passage_list, answer_list, vocabulary, html_token_set)
 
     print("DEV complite")
 
-    # train reading
     folder  = data_dir + "train/"
     files = os.listdir(folder)
     for file in files:
-    #input_file = "{}v1.0-simplified%2Fsimplified-nq-train.jsonl.gz".format(data_dir)
+        print(file)
         with gzip.open(folder+file) as f:
-            for line in f:
-                entry = json.loads(line)
-                print(type(entry))
-                print(entry.keys())
-                question = entry["question_text"]
-                q_tokens = tokenizer(question)
-                vocabulary.update({x.lemma_.lower() for x in q_tokens})
-                question_list.append(len(q_tokens))
+            q_list, p_list, a_list, vocab, html_set = process_file(folder + file)
+            question_list += q_list
+            passage_list += p_list
+            answer_list += a_list
+            vocabulary.update(vocab)
+            html_token_set.update(html_set)
 
-                passage = entry["document_text"]
-                passage_tokens = passage.split()
-                passage_text_tokens = []
-                for token in passage_tokens:
-                    if token not in html_token_set:
-                        passage_text_tokens.append(token)
-                p_tokens = tokenizer(" ".join(passage_text_tokens))
-                vocabulary.update({x.lemma_.lower() for x in p_tokens})
-                passage_list.append(len(passage_text_tokens))
+    output(question_list, passage_list, answer_list, vocabulary, html_token_set)
 
-
-                annotation = entry["annotations"][0]
-                if annotation["yes_no_answer"] != "NONE":
-                    answer_list.append(1)
-
-                else:
-                    answer_annotation = annotation["short_answers"]
-                    if len(answer_annotation) == 0:
-                        answer_annotation = annotation["long_answer"]
-                        if answer_annotation["candidate_index"] == -1:
-                            continue
-                    else:
-                        answer_annotation = answer_annotation[0]
-
-                    answer_start = answer_annotation["start_token"]
-                    answer_end = answer_annotation["end_token"]
-                    answer_list.append(answer_end - answer_start + 1)
-                break
-            break
-
-
-    print(question_list, len(question_list))
-    print(passage_list, len(passage_list))
-    print(answer_list, len(answer_list))
-    print("VOCAB", len(vocabulary))
-
-    passage_avg = sum(passage_list) / len(passage_list)
-    avg_q = sum(question_list) / len(question_list)
-    avg_a = sum(answer_list) / len(answer_list)
-
-    # & # instances	& # passages &	# A/Q & AVG Q len	& AVG P len	 & AVG A len & Vcabulary Size
-    print("&".join([str(x) for x in ["", "-", len(passage_list), "-", avg_q, passage_avg, avg_a,
-                     len(vocabulary)]]))
+    # # train reading
+    # folder  = data_dir + "train/"
+    # files = os.listdir(folder)
+    # for file in files:
+    # #input_file = "{}v1.0-simplified%2Fsimplified-nq-train.jsonl.gz".format(data_dir)
+    #     with gzip.open(folder+file) as f:
+    #         for line in f:
+    #             entry = json.loads(line)
+    #             print(type(entry))
+    #             print(entry.keys())
+    #             question = entry["question_text"]
+    #             q_tokens = tokenizer(question)
+    #             vocabulary.update({x.lemma_.lower() for x in q_tokens})
+    #             question_list.append(len(q_tokens))
+    #
+    #             passage = entry["document_text"]
+    #             passage_tokens = passage.split()
+    #             passage_text_tokens = []
+    #             for token in passage_tokens:
+    #                 if token not in html_token_set:
+    #                     passage_text_tokens.append(token)
+    #             p_tokens = tokenizer(" ".join(passage_text_tokens))
+    #             vocabulary.update({x.lemma_.lower() for x in p_tokens})
+    #             passage_list.append(len(passage_text_tokens))
+    #
+    #
+    #             annotation = entry["annotations"][0]
+    #             if annotation["yes_no_answer"] != "NONE":
+    #                 answer_list.append(1)
+    #
+    #             else:
+    #                 answer_annotation = annotation["short_answers"]
+    #                 if len(answer_annotation) == 0:
+    #                     answer_annotation = annotation["long_answer"]
+    #                     if answer_annotation["candidate_index"] == -1:
+    #                         continue
+    #                 else:
+    #                     answer_annotation = answer_annotation[0]
+    #
+    #                 answer_start = answer_annotation["start_token"]
+    #                 answer_end = answer_annotation["end_token"]
+    #                 answer_list.append(answer_end - answer_start + 1)
+    #             break
+    #         break
+    #
+    #
+    # print(question_list, len(question_list))
+    # print(passage_list, len(passage_list))
+    # print(answer_list, len(answer_list))
+    # print("VOCAB", len(vocabulary))
+    #
+    # passage_avg = sum(passage_list) / len(passage_list)
+    # avg_q = sum(question_list) / len(question_list)
+    # avg_a = sum(answer_list) / len(answer_list)
+    #
+    # # & # instances	& # passages &	# A/Q & AVG Q len	& AVG P len	 & AVG A len & Vcabulary Size
+    # print("&".join([str(x) for x in ["", "-", len(passage_list), "-", avg_q, passage_avg, avg_a,
+    #                  len(vocabulary)]]))
 
     print("writing vocabulary...")
     vocabulary_file_name = "vocabulary/NaturalQuestions.txt"
